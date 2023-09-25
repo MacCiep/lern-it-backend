f1c92e42df0f7184f83f088c6a9b6663dc358fb0
mciepluch <maciej.ciepluch@htdevelopers.com>
2023-09-26 00:52:20 +0200

    Add tests for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    subject(:user_validation) { user.valid? }
+
+    shared_examples 'invalidates the user record' do
+      it { expect(user_validation).to eq(false) }
+    end
+
+    context 'with email validation' do
+      context "when user's email is nil" do
+        let(:user) { build(:user, email: nil) }
+
+        it_behaves_like 'invalidates the user record'
+      end
+
+      context "when user's email is in the wrong format" do
+        let(:user) { build(:user, email: 'test.com') }
+
+        it_behaves_like 'invalidates the user record'
+      end
+    end
+
+    context 'with password validation' do
+      context "when user's password is nil" do
+        let(:user) { build(:user, password: nil) }
+
+        it_behaves_like 'invalidates the user record'
+      end
+
+      context "when user's password is too short" do
+        let(:user) { build(:user, password: 'test') }
+
+        it_behaves_like 'invalidates the user record'
+      end
+    end
+  end
+end
